252d3bc9396b2a32e0cb055ff9fbfb1bc0af02ca
tmpva <tmpvarcher@outlook.com>
2022-11-26 12:12:10 +0500

    Коммит с изменениями для ветки develop

--- 5-1.py
+++ 5-1.py
@@ -1,36 +1,23 @@
 import csv
 import re
-from var_dump import var_dump as vd_print
 
-
-class Salary:
-    def __init__(self, salary_from, salary_to, salary_gross, salary_currency):
-        self.salary_from = salary_from
-        self.salary_to = salary_to
-        self.salary_gross = salary_gross
-        self.salary_currency = salary_currency
-
-
-class Vacancy:
-    def __init__(self, name, description, key_skills, experience_id, premium, employer_name, salary, area_name, published_at):
-        self.name = name
-        self.description = description
-        self.key_skills = key_skills
-        self.experience_id = experience_id
-        self.premium = premium
-        self.employer_name = employer_name
-        self.salary = salary
-        self.area_name = area_name
-        self.published_at = published_at
-
-
-class DataSet:
-    def __init__(self, file_name, vacancies_objects):
-        self.file_name = file_name
-        self.vacancies_objects = vacancies_objects
-
-
-trash = {'<.*?>': '', '\s+': ' '}
+trash = { '<.*?>' : '', '\s+': ' ' }
+translation = {
+    'name' : 'Название',
+    'description' : 'Описание',
+    'key_skills' : 'Навыки',
+    'experience_id' : 'Опыт работы',
+    'premium' : 'Премиум-вакансия',
+    'employer_name' : 'Компания',
+    'salary_from' : 'Нижняя граница вилки оклада',
+    'salary_to' : 'Верхняя граница вилки оклада',
+    'salary_gross' : 'Оклад указан до вычета налогов',
+    'salary_currency' : 'Идентификатор валюты оклада',
+    'area_name' : 'Название региона',
+    'published_at' : 'Дата и время публикации вакансии',
+    'False' : 'Нет',
+    'True' : 'Да'
+}
 
 def clear_trash(string):
     clean_string = string
@@ -41,7 +28,7 @@ def clear_trash(string):
 def clear_n(string):
     return ', '.join(string.split("\n"))
 
-def clear_fields(array):
+def clear_array(array):
     cleared_array = []
     for sub_array in array:
         transport = []
@@ -53,50 +40,23 @@ def clear_fields(array):
         cleared_array.append(transport)
     return cleared_array
 
-def str_to_list(string):
-    return string.split(', ')
-
-
-def convert_to_classes(keys, values):
-    full_list = []
+def print_vacancies(keys, values):
     for element in values:
         dictionary = dict(zip(keys, element))
-        salaries = Salary(dictionary["salary_from"], dictionary["salary_to"], dictionary["salary_gross"], dictionary["salary_currency"])
-        vacancy_obj = Vacancy(dictionary["name"], dictionary["description"], str_to_list(dictionary["key_skills"]), dictionary["experience_id"],
-                              dictionary["premium"], dictionary["employer_name"], salaries, dictionary["area_name"], dictionary["published_at"])
-        full_list.append(vacancy_obj)
-    return full_list
+        for key, value in dictionary.items():
+            print("{0}: {1}".format(key, value))
+        print()
 
+vacancies = open(input(), encoding='utf-8-sig')
+csv_file = csv.reader(vacancies, delimiter=",")
 
-def get_inputs():
-    set_file = input('Введите название файла: ')
-    set_filter = input('Введите параметр фильтрации: ')
-    set_sort = input('Введите параметр сортировки: ')
-    set_order = input('Обратный порядок сортировки (Да / Нет): ')
-    set_range = input('Введите диапазон вывода: ')
-    set_columns = input('Введите требуемые столбцы: ')
+collums = [translation[i] for i in next(csv_file)]
+info = [line for line in csv_file if len(line) == len(collums) and line.count("") == 0]
 
-    return set_file, set_filter, set_sort, set_order, set_range, set_columns
+info = clear_array(info)
+for sub in info:
+    for index, item in enumerate(sub):
+        if item == 'False' or item == "True":
+            sub[index] = translation[item]
 
-
-def make_titles(file):
-    try:
-        get_titles = next(file)
-        return get_titles
-    except StopIteration:
-        print("Пустой файл")
-        exit()
-
-
-def main():
-    file_name, filter_set, sort_by, order, output_range, columns = get_inputs()
-    csv_file = csv.reader(open(file_name, encoding='utf-8-sig'), delimiter=",")
-    titles = make_titles(csv_file)
-    fields = clear_fields([l for l in csv_file if len(l) == len(titles) and l.count("") == 0])
-    vacancies = convert_to_classes(titles, fields)
-
-    vd_print(DataSet(file_name, vacancies))
-
-
-if __name__ == "__main__":
-    main()
+print_vacancies(collums, info)
