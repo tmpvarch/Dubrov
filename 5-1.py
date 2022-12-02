86089053a53602e9c8a51b467c6fdf86365acedf
tmpva <tmpvarcher@outlook.com>
2022-12-02 19:32:52 +0500

    изменилось что-то...

--- 5-1.py
+++ 5-1.py
@@ -3,7 +3,7 @@ import re
 
 trash = { '<.*?>' : '', '\s+': ' ' }
 translation = {
-    'name' : 'Название',
+    'name' : 'Имя',
     'description' : 'Описание',
     'key_skills' : 'Навыки',
     'experience_id' : 'Опыт работы',
